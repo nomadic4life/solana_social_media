555e11e2e1623ca1821c3dc1117f830cfd2393ed
nomadic4life <orbison.dennis@gmail.com>
2024-03-04 00:42:36 -0800

    implemented validation logic

--- programs/solana_social_media/src/lib.rs
+++ programs/solana_social_media/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::syscalls};
 
 declare_id!("59chiAcJ99ttxaDLxcsJh8Be2tChirZfWsWxkd2SRGDj");
 
@@ -12,7 +12,317 @@ pub mod solana_social_media {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init, 
+        payer = authority,
+        space = Senddit::LEN,
+        seeds = [
+            b"senddit"
+        ],
+        bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitPostStore<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"senddit"
+        ],
+        bump = senddit.bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    /// CHECK: Account must match our config
+    #[account(
+        mut,
+        address = senddit.treasury
+    )]
+    pub treasury: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = PostStore::LEN,
+        seeds = [
+            (((Clock::get().unwrap().unix_timestamp.abs() as f64) / (60.0 * 60.0 * 24.0)) as u128).to_string().as_bytes().as_ref(),
+        ], 
+        bump
+    )]
+    pub post_store: Account<'info, PostStore>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(link: String)]
+pub struct PostLink<'info> {
+    #[account(
+        mut,
+        seeds = [b"senddit"],
+        bump = senddit.bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    /// CHECK: Acocunt must match our config
+    #[account(
+        mut,
+        address = senddit.treasury
+    )]
+    pub treasury: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+
+    /// CHEKC: Account must match the creator of the store
+    #[account(
+        mut,
+        address = post_store.authority
+    )]
+    pub poster_wallet: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            (((Clock::get().unwrap().unix_timestamp.abs() as f64) / (60.0 * 60.0 * 24.0)) as u128).to_string().as_bytes().as_ref(),
+        ],
+        bump = post_store.bump
+    )]
+    pub post_store: Account<'info, PostStore>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = Post::LEN,
+        seeds = [
+            post_store.key().as_ref(),
+            (post_store.posts + 1).to_string().as_bytes().as_ref()
+        ],
+        bump
+    )]
+    pub post: Account<'info, Post>,
+
+    /// CHECK: It's okay not to deserialize this, its just to prvent duplicate links 
+    #[account(
+        init,
+        payer = authority,
+        space = 8,
+        seeds = [
+            link.as_bytes().as_ref()
+        ],
+         bump
+    )]
+    pub post_pda: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(number: String)]
+pub struct UpvotePost<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"senddit"
+        ],
+        bump = senddit.bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    /// CHECK:: Account must match our config
+    #[account(
+        mut,
+        address = senddit.treasury
+    )]
+    pub treasury: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    /// CHECK: Account must match the maker of the post
+    #[account(
+        mut,
+        address = post.authority
+    )]
+    pub poster_wallet: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            // incorrect seed. use time stamp
+            post.key().as_ref()
+        ], 
+        bump = post_store.bump
+    )]
+    pub post_store: Account<'info, PostStore>,
+
+    #[account(
+        mut,
+        seeds = [
+            post_store.key().as_ref(),
+            number.as_bytes().as_ref(),
+        ],
+        bump = post.bump
+    )]
+    post: Account<'info, Post>,
+
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct InitCommentStore<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"senddit"
+        ],
+        bump = senddit.bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    /// CHECK: Account must match our config
+    #[account(
+        mut,
+        address = senddit.treasury
+    )]
+    pub treasury: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = CommentStore::LEN,
+        seeds = [
+            post.key().as_ref()
+        ],
+        bump
+    )]
+    pub comment_store: Account<'info, CommentStore>,
+
+    pub post: Account<'info, Post>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct PostComment<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"senddit"
+        ],
+        bump = senddit.bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    /// CHECK: Account must match our config
+    #[account(
+        mut,
+        address = senddit.treasury
+    )]
+    pub treasury: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    /// CHECK: Account must match the creator of the store
+    #[account(
+        mut, 
+        address = comment_store.authority
+    )]
+    pub commenter_wallet: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            post.key().as_ref()
+        ],
+        bump =comment_store.bump
+    )]
+    pub comment_store: Account<'info, CommentStore>,
+
+    #[account(mut)]
+    pub post: Account<'info, Post>,
+
+    #[account(
+        init,
+        payer = authority,
+        space = Comment::LEN,
+        seeds = [
+            comment_store.key().as_ref(),
+            (comment_store.comments + 1).to_string().as_bytes().as_ref()
+        ],
+        bump
+    )]
+    pub comment: Account<'info, Comment>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(number: String)]
+pub struct UpvoteComment<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"senddit"
+        ],
+        bump = senddit.bump
+    )]
+    pub senddit: Account<'info, Senddit>,
+
+    /// CHECK: Account must match our config
+    #[account(
+        mut,
+        address = senddit.treasury
+    )]
+    pub treasury: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    /// CHECK: Account must match the maker of the comments
+    #[account(
+        mut,
+        address = comment.authority
+    )]
+    pub commenter_wallet: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            post.key().as_ref()
+        ],
+        bump = comment_store.bump
+    )]
+    pub comment_store: Account<'info, CommentStore>,
+
+    #[account(mut)]
+    pub post: Account<'info, Post>,
+
+    #[account(
+        mut,
+        seeds = [
+            comment_store.key().as_ref(),
+            number.as_bytes().as_ref()
+        ],
+        bump = comment.bump
+    )]
+    pub comment: Account<'info, Comment>,
+
+    pub system_program: Program<'info, System>,
+}
+
 
 // create pointer to content
 // like content
@@ -68,7 +378,7 @@ impl Post {
 #[account]
 pub struct CommentStore {
     pub authority: Pubkey,
-    pub posts: u128,
+    pub comments: u128,
     pub bump: u8,
 }
 
@@ -79,7 +389,7 @@ impl CommentStore {
 #[account]
 pub struct Comment {
     pub authority: Pubkey,
-    pub link: String,
+    pub comment: String,
     pub upvotes: u64,
     pub comments: u64,
     pub bump: u8,
